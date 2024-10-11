90a362a46749e56ea6adf21b132948f6bd91640c
yabing <yabingc3@gmail.com>
2024-10-11 19:44:54 +0200

    models for pong created

--- backend/pong/game/models.py
+++ backend/pong/game/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from datetime import timedelta
-from auth.authentication.models import User
+
+from django.forms import JSONField
+
+from .validators import validate_start_date
 
 class Match(models.Model):
-	match_type = models.CharField(blank=False, null=False)
+	match_type = models.CharField(blank=False, null=False, choices=['Quick', 'Standard'])
 	created_at = models.DateTimeField(auto_now_add=True)
-	duration = models.DurationField(default=timedelta(minutes=2))
+	duration = models.DurationField(blank=True, default=timedelta(minutes=2))
 	player_1 = models.CharField(length=30, blank=False, null=False)
 	player_2 = models.CharField(length=30, blank=False, null=False)
 	score_player_1 = models.IntegerField(default=0)
@@ -15,31 +18,40 @@ class Match(models.Model):
 	winner = models.CharField(null=True)
 
 	def __str__(self):
-		return f"Match started at {self.created_at}"
+		return f"{self.match_type} Match started at {self.created_at}"
 	
 class Tournament(models.Model):
-	name = models.CharField(max_length=100)
-	players = models.ManyToManyField(User, related_name='tournaments')
+	name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	start_date = models.DateTimeField(blank=False, null=False)
+	end_date = models.DateTimeField(blank=False, null=False)
+	players_required = models.PositiveIntegerField()
+	match_duration = models.DurationField(blank=True, default=timedelta(minutes=2))
+	status = models.CharField(choices=['pending', 'ongoing', 'finished'], default='pending')
+	players = models.JSONField(default=dict, blank=True, null=True)
 	current_match = models.ForeignKey(Match, null=True)
+
+	def clean(self):
+		super().clean()
+		validate_start_date(self.created_at, self.start_date)
+
+	def save(self, *args, **kwargs):
+		self.clean()
+		super().save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
 	
 class Matchmaking(models.Model):
-    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
-    next_match = models.ForeignKey(Match, on_delete=models.SET_NULL, null=True, blank=True)
-    announced_match = models.BooleanField(default=False)
+	tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='matchmaking_set')
+	players_queued = models.JSONField(defaut=list, blank=True, null=True)
+	matchs = JSONField(default=list, blank=True, null=True)
+	next_match = models.ForeignKey(Match, on_delete=models.SET_NULL, null=True, blank=True)
 
-    def __str__(self):
-        return f"Matchmaking for {self.tournament.name} (ID: {self.id})"
+	# def add_player():
+	# def create_match():
+	# def set_next_match():
 
-class MatchOrder(models.Model):
-    matchmaking = models.ForeignKey(Matchmaking, on_delete=models.CASCADE)
-    match = models.ForeignKey(Match, on_delete=models.CASCADE)
-    order = models.PositiveIntegerField()
+	def __str__(self):
+		return f"Matchmaking for {self.tournament.name}"
 
-    class Meta:
-        ordering = ['order']
-
-    def __str__(self):
-        return f"Match {self.match.id} in Matchmaking {self.matchmaking.id} at position {self.order}"

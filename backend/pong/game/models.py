4ffe318978f8ac61e59b16c9cc7ab014a1b2755c
yabing <yabingc3@gmail.com>
2024-10-11 14:09:50 +0200

    conception des modeles

--- backend/pong/game/models.py
+++ backend/pong/game/models.py
@@ -1,37 +1,45 @@
 from django.db import models
+from datetime import timedelta
 from auth.authentication.models import User
 
-class BaseMatch(models.Model):
+class Match(models.Model):
+	match_type = models.CharField(blank=False, null=False)
 	created_at = models.DateTimeField(auto_now_add=True)
-	duration = models.DurationField(null=True, blank=True)
+	duration = models.DurationField(default=timedelta(minutes=2))
+	player_1 = models.CharField(length=30, blank=False, null=False)
+	player_2 = models.CharField(length=30, blank=False, null=False)
 	score_player_1 = models.IntegerField(default=0)
 	score_player_2 = models.IntegerField(default=0)
 	# ball position, ball speed, paddle position
 	game_data = models.JSONField(default=dict, blank=True)
+	winner = models.CharField(null=True)
 
-	class Meta:
-		abstract = True
-	
 	def __str__(self):
 		return f"Match started at {self.created_at}"
 	
-class QuickMatch(BaseMatch):
-	def __str__(self):
-		return f"Quick Match ({self.created_at})"
-	
-class ConnectedMatch(BaseMatch):
-	player_1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='connected_player_1')
-	player_2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='connected_player_2')
-
-	def __str__(self):
-		return f"Connected Match between {self.player_1} and {self.player_2} ({self.created_at})"
-	
-class Tournament(BaseMatch):
+class Tournament(models.Model):
 	name = models.CharField(max_length=100)
-	start_date = models.DateTimeField()
-	end_date = models.DateTimeField(null=True, blank=True)
-	participants = models.ManyToManyField(User, related_name='tournaments')
-	status = models.CharField(max_length=20, choices=[('upcoming', 'Upcoming'), ('ongoing', 'Ongoing'), ('finished', 'Finished')], default='upcoming')
+	players = models.ManyToManyField(User, related_name='tournaments')
+	current_match = models.ForeignKey(Match, null=True)
 
 	def __str__(self):
 		return self.name
+	
+class Matchmaking(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    next_match = models.ForeignKey(Match, on_delete=models.SET_NULL, null=True, blank=True)
+    announced_match = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Matchmaking for {self.tournament.name} (ID: {self.id})"
+
+class MatchOrder(models.Model):
+    matchmaking = models.ForeignKey(Matchmaking, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"Match {self.match.id} in Matchmaking {self.matchmaking.id} at position {self.order}"
--- backend/pong/game/urls.py
+++ backend/pong/game/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('quick-match/', views.QuickMatch.as_view(), name='quick_match'),
 	path('connected-match/', views.connectedmatch.as_view(), name='connected_match')
+	path('tournament/', views.tournament.as_view(), name='tournament')
 ]
